6eb277ee1647bfbe1dcf186b44e46911497e4afb
MattDeDuck <Minihobbsch@gmail.com>
2022-02-11 15:06:58 +0000

    Fixed shelf issue in the shop

--- Plugin.cs
+++ Plugin.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace Shelves
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, "1.0.0.0")]
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, "1.0.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource Log { get; set; }
@@ -17,7 +17,7 @@ namespace Shelves
 
         // List of locations for the rooms
         public static Vector3[] shelvesBedroom = new Vector3[] { new Vector3(-9.1f, 1.6f, 0f), new Vector3(4.5f, 1f, 0f) };
-        public static Vector3[] shelvesMeeting = new Vector3[] { new Vector3(-8f, 5f, 0f), new Vector3(-8f, 2.5f, 0f), new Vector3(-9.9f, -5.73f, 0f), new Vector3(-5f, -5.73f, 0f) };        
+        public static Vector3[] shelvesMeeting = new Vector3[] { new Vector3(-5f, 1f, 0f), new Vector3(-5f, -5.73f, 0f) };        
         public static Vector3[] shelvesLab = new Vector3[] { new Vector3(-9.9f, -5.73f, 0f), new Vector3(-5f, -5.73f, 0f) };
         public static Vector3[] shelvesBasement = new Vector3[] { new Vector3(3.7f, 3.5f, 0f), new Vector3(-0.6f, 5f, 0f) };
 
